478a3819dd3543657c8941a16211cd36e0aacf08
Ritesh Singh <riteshsinghmatiyani74@gmail.com>
2024-08-17 10:47:43 +0530

    Signed-off-by: Ritesh Singh <riteshsinghmatiyani74@gmail.com>

---
+++ New_Jenkins_Project/src/Jenkins/Name.java
@@ -0,0 +1,12 @@
+package Jenkins;
+
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("My New Jenkins Project");
+	}
+
+}
